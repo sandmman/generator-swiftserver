b37d2548c9d1ddd6cc19f75f64bf2f1fb317054f
Neil Hardman <nhardman@uk.ibm.com>
2017-11-03 13:51:41 +0000

    test 1: identify npm key

--- release.sh
+++ release.sh
@@ -1,16 +1,17 @@
 # Script to release to NPM and publish tag
 
-git remote rm origin
-git remote add origin https://SwiftDevOps:${GH_TOKEN}@github.com/IBM-Swift/generator-swiftserver
+#git remote rm origin
+#git remote add origin https://SwiftDevOps:${GH_TOKEN}@github.com/IBM-Swift/generator-swiftserver
 VERSION=`node -e "console.log(require('./package.json').version);"`
 echo "Creating release ${VERSION}" 
-git tag $VERSION && git push origin $VERSION || true
+#git tag $VERSION && git push origin $VERSION || true
 # Merge back into develop and push those changes
-git fetch origin && git checkout develop && git merge origin/master && git push origin develop
-# npm publish 
+#git fetch origin && git checkout develop && git merge origin/master && git push origin develop
+echo npm publish 
+echo "NPM_TOKEN=${NPM_TOKEN}"
 
 # Deleting the old release branch
-BRANCH_TO_DELETE=updateTo$VERSION
-echo Deleting $BRANCH_TO_DELETE 
-curl -u SwiftDevOps:${GH_TOKEN} -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$BRANCH_TO_DELETE/protection"
-git push origin :$BRANCH_TO_DELETE
+#BRANCH_TO_DELETE=updateTo$VERSION
+#echo Deleting $BRANCH_TO_DELETE 
+#curl -u SwiftDevOps:${GH_TOKEN} -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$BRANCH_TO_DELETE/protection"
+#git push origin :$BRANCH_TO_DELETE
